911447246ff0b888462a3151325a967115855a36
MSyafiqYukinanda <syukinanda@gmail.com>
2021-04-25 02:29:56 +0700

    list user, list admin, list transaksi & topup (finance), ERROR grafik

--- src/pages/admin/admins.js
+++ src/pages/admin/admins.js
@@ -6,6 +6,9 @@ import Container from '@material-ui/core/Container'
 import { DataGrid } from '@material-ui/data-grid';
 import Typography from '@material-ui/core/Typography'
 
+import { connect } from 'react-redux'
+import { get_admin_data } from '../../../static/utils/redux/Actions/user'
+
 const columns = [
     { field: 'name', headerName: 'Nama', width: 300 },
     { field: 'email', headerName: 'Email', width: 300 },
@@ -22,20 +25,40 @@ const rows = [
     createData(4, 'admin4', 'adm4@email.com'),
 ];
 const Component = props => {
+    const [rows,setRows] = React.useState([])
+
+    React.useEffect(() => {
+        get_admin_data(props.dispatch)
+    },[]) 
+
+    React.useEffect(() => {
+        if (props.users){
+            function mapUser(user,index) {
+                return (createData(index+1, user.name, user.email))
+            }
+            let row = props.users.map(mapUser)
+            setRows(row)
+        }
+    },[props.users]) 
+
     return(
         <Layout>
             <SEO title='admins' />
             <Container>
                 <Typography align='center' variant='h2' gutterBottom style={{ fontWeight: 'bold' }}>
-                    List User
+                    List Admin
                 </Typography>
                 
-                    <div style={{ margin:'3em 0',height: 300, width: '100%'}}>
-                        <DataGrid rows={rows} columns={columns} pageSize={5} />
-                    </div>
+                <div style={{ margin:'3em 0',height: 300, width: '100%'}}>
+                    <DataGrid rows={rows} columns={columns} pageSize={5} />
+                </div>
             </Container>
         </Layout>
     )
 }
 
-export default Component
+export default connect(state => ({
+    error: state.user.error,
+    isLoading: state.user.loading,
+    users: state.user.users
+}), null)(Component)
--- src/pages/admin/users.js
+++ src/pages/admin/users.js
@@ -6,6 +6,9 @@ import Container from '@material-ui/core/Container'
 import { DataGrid } from '@material-ui/data-grid';
 import Typography from '@material-ui/core/Typography'
 
+import { connect } from 'react-redux'
+import { get_user_data } from '../../../static/utils/redux/Actions/user'
+
 const columns = [
     { field: 'name', headerName: 'Nama', width: 300 },
     { field: 'email', headerName: 'Email', width: 300 },
@@ -15,26 +18,41 @@ function createData(id, name, email) {
     return { id, name, email};
 }
 
-const rows = [
-    createData(1, 'user1', 'usr1@email.com'),
-    createData(2, 'user2', 'usr2@email.com'),
-    createData(3, 'user3', 'usr3@email.com'),
-    createData(4, 'user4', 'usr4@email.com'),
-];
+
 const Component = props => {
+    const [rows,setRows] = React.useState([])
+
+    React.useEffect(() => {
+        get_user_data(props.dispatch)
+    },[]) 
+
+    React.useEffect(() => {
+        if (props.users){
+            function mapUser(user,index) {
+                return (createData(index+1, user.name, user.email))
+            }
+            let row = props.users.map(mapUser)
+            setRows(row)
+        }
+    },[props.users]) 
+
     return(
-        <Layout>
-            <SEO title='users' />
-            <Container>
-                <Typography align='center' variant='h2' gutterBottom style={{ fontWeight: 'bold' }}>
-                    List User
-                </Typography>
+            <Layout>
+                <SEO title='users' />
+                <Container>
+                    <Typography align='center' variant='h2' gutterBottom style={{ fontWeight: 'bold' }}>
+                        List User
+                    </Typography>
                     <div style={{ margin:'3em 0', height: 300, width: '100%'}}>
                         <DataGrid rows={rows} columns={columns} pageSize={5} />
                     </div>
-            </Container>
-        </Layout>
-    )
-}
+                </Container>
+            </Layout>
+        )
+    }
 
-export default Component
+export default connect(state => ({
+    error: state.user.error,
+    isLoading: state.user.loading,
+    users: state.user.users
+}), null)(Component)
